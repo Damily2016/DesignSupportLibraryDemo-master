b9d889c51d7ef225d8269de61006acafde0ed9e6
V.Wenju.Tian <V.Wenju.Tian.@deltaww.com>
2016-11-10 13:44:38 +0800

    修改

--- app/src/main/java/com/hujiang/designsupportlibrarydemo/MainActivity.java
+++ app/src/main/java/com/hujiang/designsupportlibrarydemo/MainActivity.java
@@ -1,13 +1,11 @@
 package com.hujiang.designsupportlibrarydemo;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
-import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -18,18 +16,22 @@ import android.view.View;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
-import com.hujiang.designsupportlibrarydemo.fragments.FirstTabFragment;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.hujiang.designsupportlibrarydemo.catory.CatoryFragment;
+import com.hujiang.designsupportlibrarydemo.home.FirstTabFragment;
+import com.hujiang.designsupportlibrarydemo.home.HomeFragment;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout mDrawerLayout;
-    private ViewPager mViewPager;
     private TabLayout mTabLayout;
     private Toolbar toolbar;
-    private RadioButton main_tab_home,main_tab_catagory,mdin_tab_car;
+    private RadioButton main_tab_home, main_tab_catagory, mdin_tab_car;
+    private FragmentManager fragmentManager;
+    private TabLayout tabLayout;
+
+    public TabLayout getTabLayout() {
+        return tabLayout;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +40,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
        /* FragmentManager fragmentManager = getSupportFragmentManager();
         Fragment homeFragment = new HomeFragment();
         fragmentManager.beginTransaction().replace(R.id.container_content, homeFragment).commit();*/
-       initView();
+        initView();
+
 
         toolbar = (Toolbar) findViewById(R.id.toolbar1);
+        tabLayout = ((TabLayout) findViewById(R.id.tabs));
+
         mDrawerLayout = (DrawerLayout) findViewById(R.id.dl_main_drawer);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
             @Override
@@ -59,12 +64,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-           @Override
-           public boolean onMenuItemClick(MenuItem item) {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
 
-               return true;
-           }
-       });
+                return true;
+            }
+        });
 
       /*  final ActionBar ab = getSupportActionBar();
         ab.setHomeAsUpIndicator(R.drawable.ic_menu);
@@ -76,21 +81,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (navigationView != null) {
             setupDrawerContent(navigationView);
         }*/
-
+        fragmentManager = getSupportFragmentManager();
         mDrawerLayout.setDrawerListener(toggle);
         toggle.syncState();
         NavigationView navigationView =
                 (NavigationView) findViewById(R.id.nv_main_navigation);
         navigationView.setNavigationItemSelectedListener(this);
-        mViewPager = (ViewPager) findViewById(R.id.viewpager);
-        setupViewPager();
+        Fragment homeFragment = new HomeFragment();
+        fragmentManager.beginTransaction().replace(R.id.container_content, homeFragment).commit();
 
     }
 
     private void initView() {
-        main_tab_home= (RadioButton) findViewById(R.id.main_tab_home);
-        main_tab_catagory= (RadioButton) findViewById(R.id.main_tab_catagory);
-        mdin_tab_car= (RadioButton) findViewById(R.id.mdin_tab_car);
+        main_tab_home = (RadioButton) findViewById(R.id.main_tab_home);
+        main_tab_catagory = (RadioButton) findViewById(R.id.main_tab_catagory);
+        mdin_tab_car = (RadioButton) findViewById(R.id.mdin_tab_car);
         main_tab_home.setOnClickListener(this);
         main_tab_catagory.setOnClickListener(this);
         mdin_tab_car.setOnClickListener(this);
@@ -112,28 +117,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return super.onOptionsItemSelected(item);
     }
 
-    private void setupViewPager() {
-        mTabLayout = (TabLayout) findViewById(R.id.tabs);
-        List<String> titles = new ArrayList<>();
-        titles.add("进行中");
-        titles.add("已完成");
-        titles.add("未开始");
-        titles.add("关注");
-        mTabLayout.addTab(mTabLayout.newTab().setText(titles.get(0)));
-        mTabLayout.addTab(mTabLayout.newTab().setText(titles.get(1)));
-        mTabLayout.addTab(mTabLayout.newTab().setText(titles.get(2)));
-        mTabLayout.addTab(mTabLayout.newTab().setText(titles.get(3)));
-        List<Fragment> fragments = new ArrayList<>();
-        fragments.add(new ListFragment());
-        fragments.add(new FirstTabFragment());
-        fragments.add(new ListFragment());
-        fragments.add(new ListFragment());
-        FragmentAdapter adapter =
-                new FragmentAdapter(getSupportFragmentManager(), fragments, titles);
-        mViewPager.setAdapter(adapter);
-        mTabLayout.setupWithViewPager(mViewPager);
-        mTabLayout.setTabsFromPagerAdapter(adapter);
-    }
+
 
 /*    private void setupDrawerContent(NavigationView navigationView) {
         navigationView.setNavigationItemSelectedListener(
@@ -152,9 +136,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (menuItem.getItemId()) {
             case R.id.nav_home:
               /*  toolbar.setTitle("首页");*/
-                FragmentManager fmMenuFirst = getSupportFragmentManager();
+
                 Fragment homeFragment = new HomeFragment();
-                fmMenuFirst.beginTransaction().replace(R.id.content_main, homeFragment).commit();
+                fragmentManager.beginTransaction().replace(R.id.container_content, homeFragment).commit();
 
                 break;
         }
@@ -164,20 +148,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-switch (v.getId()){
-    case R.id.main_tab_home:
-        break;
-    case R.id.main_tab_catagory:
-        Intent secondIntent=new Intent(this,SecondTabActivity.class);
-       startActivity(secondIntent);
-        break;
-    case R.id.mdin_tab_car:
-       FragmentManager fm=getSupportFragmentManager();
-        Fragment FirstTa=new FirstTabFragment();
-        fm.beginTransaction().replace(R.id.content_main,FirstTa,null);
+        switch (v.getId()) {
+            case R.id.main_tab_home:
+                Fragment homeFragment = new HomeFragment();
+                fragmentManager.beginTransaction().replace(R.id.container_content, homeFragment).commit();
 
-        break;
+                break;
+            case R.id.main_tab_catagory:
+                CatoryFragment fragment = new CatoryFragment();
+                fragmentManager.beginTransaction().replace(R.id.container_content, fragment).commit();
+                break;
+            case R.id.mdin_tab_car:
+                FragmentManager fm = getSupportFragmentManager();
+                Fragment FirstTa = new FirstTabFragment();
+                fm.beginTransaction().replace(R.id.container_content, FirstTa, null);
 
-}
+                break;
+
+        }
     }
 }
--- app/src/main/java/com/hujiang/designsupportlibrarydemo/home/ThirdTabActivity.java
+++ app/src/main/java/com/hujiang/designsupportlibrarydemo/home/ThirdTabActivity.java
@@ -1,8 +1,10 @@
-package com.hujiang.designsupportlibrarydemo;
+package com.hujiang.designsupportlibrarydemo.home;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.hujiang.designsupportlibrarydemo.R;
 
 /**
  * Created by Dandan.Cao on 2016/11/7.
